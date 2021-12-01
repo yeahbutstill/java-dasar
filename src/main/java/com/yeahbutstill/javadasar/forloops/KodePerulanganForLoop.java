4d82b09d35a87f05d00bf893658b003517a9449a
Dani Setiawan <mayadani28@gmail.com>
2021-12-01 15:02:10 +0700

    For Loop Kondisi, Init Statement, Post Statement

--- src/main/java/com/yeahbutstill/javadasar/forloops/KodePerulanganForLoop.java
+++ src/main/java/com/yeahbutstill/javadasar/forloops/KodePerulanganForLoop.java
@@ -2,8 +2,28 @@ package com.yeahbutstill.javadasar.forloops;
 
 public class KodePerulanganForLoop {
     public static void main(String[] args) {
-        for (;;) {
-            System.out.println("Please Stop");
+        // Tanpa henti
+//        for (;;) {
+//            System.out.println("Please Stop");
+//        }
+
+        // menggunakan Kondisi
+//        var count = 1;
+//        for (; count <= 10;) {
+//            System.out.println("Perulangan ke- " + count);
+//            count++;
+//        }
+
+        // menggunakan Init Statement
+//        for (var count = 1; count <= 10;) {
+//            System.out.println("Perulangan ke-" + count);
+//            count++;
+//        }
+
+        // menggunakan Post Statement
+        for (var count = 1; count <= 10; count++) {
+            System.out.println("Perulangan Ke-" + count);
         }
+        
     }
 }
